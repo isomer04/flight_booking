5fe577dcbbed5b4bc088f0d1185c0cbd47a9e706
isomer04 <43922158+isomer04@users.noreply.github.com>
2024-02-23 08:24:22 -0500

    Created initial components

---
+++ src/app/pages/website/bookflight/bookflight.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookflight',
+  templateUrl: './bookflight.component.html',
+  styleUrl: './bookflight.component.css'
+})
+export class BookflightComponent {
+
+}
